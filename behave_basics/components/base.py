9a3ceb87fad79eaa26dad8a1759685ac23057afd
Dmitrii Vladimirov <dvlad7909@gmail.com>
2023-12-28 19:43:35 -0800

    behave exercise

--- behave_basics/components/base.py
+++ behave_basics/components/base.py
@@ -30,5 +30,5 @@ class Base:
             print(f"Search text '{search_text.lower()}', is not in header '{element.text.lower()}'")
 
     def get_text(self, locator):
-        element = WebDriverWait(self.driver, 0.1).until(EC.visibility_of_element_located(locator))
+        element = WebDriverWait(self.driver, 1).until(EC.visibility_of_element_located(locator))
         return element.text
